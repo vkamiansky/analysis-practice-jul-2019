b3d4dfc1813ef02171f5829d961ac5cf5a4f8902
Shurron <antr01@yandex.ru>
2019-06-27 13:38:54 +0300

    Added configuration file

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
-﻿namespace QuizResults
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace QuizResults
 {
 	class Program
 	{
+		static IConfigurationRoot BuildConfiguration()
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json");
+
+			return builder.Build();
+		}
+
 		static void Main(string[] args)
 		{
 			var parser = new Parser();
-			var holyJS = parser.ParseFile("../../../holyjs.csv");
-			var dotnext = parser.ParseFile("../../../dotnext.csv");
+			var config = BuildConfiguration();
+			var data = parser.ParseFile(config["DataFilePath"]);
 		}
 	}
 }
