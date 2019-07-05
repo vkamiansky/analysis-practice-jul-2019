7a9d818b0ad1802a163502c0a10df9391df8c1b7
Shurron <antr01@yandex.ru>
2019-07-05 16:32:45 +0300

    Added one test

---
+++ test/CsvParserTests.cs
@@ -0,0 +1,131 @@
+using System;
+using Xunit;
+using QuizResults;
+using QuizResults.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace QuizResults.Tests
+{
+	public class CsvParserTests
+	{
+		internal void CompareAnswers(Answer expected, Answer actual)
+		{
+			Assert.Equal(expected.Question.QuestionText, actual.Question.QuestionText);
+			Assert.Equal(expected.Question.AnswersList.Count, actual.Question.AnswersList.Count);
+			for (var i = 0; i < actual.Question.AnswersList.Count; i++)
+			{
+				Assert.Equal(expected.Question.AnswersList.ElementAt(i), actual.Question.AnswersList.ElementAt(i));
+			}
+			Assert.Equal(expected.AnswerIndex, actual.AnswerIndex);
+			Assert.Equal(expected.Question.CorrectAnswerIndex, actual.Question.CorrectAnswerIndex);
+		}
+
+		[Fact]
+		public void OneCorrectTest()
+		{
+			var parser = new CsvParser();
+			var data = parser.ParseFile("Tests/OneCorrectTest.txt");
+
+			Assert.Single(data);
+			var test = data.First();
+
+			Assert.Equal("email@site.com", test.Person.Email);
+			Assert.Equal("Иван", test.Person.Name);
+			Assert.Null(test.Notes);
+			Assert.Equal((uint)60, test.Result);
+
+			Assert.Equal(5, test.Answers.Count);
+
+			var expected = new Answer
+			{
+				Question = new Question
+				{
+					QuestionText = "Мейстеры Цитадели изобрели бургеры и спешат поделиться своим открытием со всеми. Но так ли хорош рецепт?",
+					AnswersList = new List<string>
+					{
+						"bread bread burger cheese",
+						"bread burger cheese bread",
+						"bread burger bread cheese",
+						"burger cheese bread bread"
+					},
+					CorrectAnswerIndex = 2
+				},
+				AnswerIndex = 2
+			};
+			CompareAnswers(expected, test.Answers.ElementAt(0));
+
+			expected = new Answer
+			{
+				Question = new Question
+				{
+					QuestionText = "Багет - это хлеб, а хлеб ? это багет?",
+					AnswersList = new List<string>
+					{
+						"undefined true",
+						"false true",
+						"true true",
+						"true undefined"
+					},
+					CorrectAnswerIndex = 0
+				},
+				AnswerIndex = 3
+			};
+			CompareAnswers(expected, test.Answers.ElementAt(1));
+
+			expected = new Answer
+			{
+				Question = new Question
+				{
+					QuestionText = "Путник знает, что и как заказать в таверне.",
+					AnswersList = new List<string>
+					{
+						"[ 'bacon and eggs' ]",
+						"[ 'bacon and eggs', 'coffee' ]",
+						"[ 'coffee', 'bacon and eggs' ]",
+						"SyntaxError: Unexpected token"
+					},
+					CorrectAnswerIndex = 0
+				},
+				AnswerIndex = 0
+			};
+			CompareAnswers(expected, test.Answers.ElementAt(2));
+
+			expected = new Answer
+			{
+				Question = new Question
+				{
+					QuestionText = "Тормунд и Джон решили покушать бутербродов. Джон маленький и хочет скушать 2 бутерброда, а Тормунд большой и хочет 4. Но у Джона только 1 кусочек хлеба, а у Тормунда - только 3. Сколько кусочков хлеба им не хватает?",
+					AnswersList = new List<string>
+					{
+						"'1-23-4'",
+						"-2",
+						"'1-2-1'",
+						"'-13-4'"
+					},
+					CorrectAnswerIndex = 2
+				},
+				AnswerIndex = 2
+			};
+			CompareAnswers(expected, test.Answers.ElementAt(3));
+
+			expected = new Answer
+			{
+				Question = new Question
+				{
+					QuestionText = "Мелисандра хорошо управляется с магией. Получится ли у нас? Что будет выведено в консоль после расфокусировки input элемента?",
+					AnswersList = new List<string>
+					{
+						"focus pocus",
+						"focus",
+						"ничего",
+						"undefined"
+					},
+					CorrectAnswerIndex = 2
+				},
+				AnswerIndex = 0
+			};
+			CompareAnswers(expected, test.Answers.ElementAt(4));
+		}
+	}
+}
