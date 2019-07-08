1cc5cacf371ff0a74888c8ac3e95fd1a7b979fcf
Shurron <antr01@yandex.ru>
2019-07-08 15:31:56 +0300

    Refactoring

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace QuizResults
 {
@@ -28,78 +26,26 @@ namespace QuizResults
 				Console.WriteLine(parser.ErrorMessage);
 			}
 
-			// Collection of pair <Email, Number of attempts>
-			var attemptsNumber = new Dictionary<string, int>();
+			var report = DataAnalyzer.Analyze(data);
 
-			// Collection of pair <Result, Amount>
-			var resultDistribution = new Dictionary<uint, uint>();
-
-			var totalAmount = 0;
-			var maxNumberOfAttempts = 0;
-			var personWithMaxNumberOfAttempts = "";
-
-			foreach (var testResult in data)
-			{
-				if (!attemptsNumber.ContainsKey(testResult.Person.Email))
-					attemptsNumber.Add(testResult.Person.Email, 0);
-				attemptsNumber[testResult.Person.Email]++;
-
-				if (attemptsNumber[testResult.Person.Email] > maxNumberOfAttempts)
-				{
-					maxNumberOfAttempts = attemptsNumber[testResult.Person.Email];
-					personWithMaxNumberOfAttempts = testResult.Person.Email;
-				}
-
-				if (!resultDistribution.ContainsKey(testResult.Result))
-					resultDistribution.Add(testResult.Result, 0);
-				resultDistribution[testResult.Result]++;
-
-				totalAmount++;
-			}
-
-			Console.WriteLine("Всего тестов: {0}", totalAmount);
-			Console.WriteLine("Количество уникальных e-mail'ов: {0}", attemptsNumber.Count);
-
-			var attemptDistribution = new uint[maxNumberOfAttempts];
-			foreach (var el in attemptsNumber)
-			{
-				attemptDistribution[el.Value - 1]++;
-			}
+			Console.WriteLine("Всего тестов: {0}", report.TotalAmountOfTests);
+			Console.WriteLine("Количество уникальных e-mail'ов: {0}", report.AmountOfUniqueEmails);
 
 			Console.WriteLine("Распределение попыток:");
-			for (var i = 0; i < attemptDistribution.Length; i++)
+			for (var i = 0; i < report.AttemptDistribution.Length; i++)
 			{
-				if (attemptDistribution[i] != 0)
-					Console.WriteLine("{0}: {1}", i + 1, attemptDistribution[i]);
+				if (report.AttemptDistribution[i] != 0)
+					Console.WriteLine("{0}: {1}", i + 1, report.AttemptDistribution[i]);
 			}
 
 			Console.WriteLine("Распределение результатов:");
-			foreach (var el in resultDistribution)
+			foreach (var el in report.ResultDistribution)
 			{
 				Console.WriteLine("{0}: {1}", el.Key, el.Value);
 			}
 
-			Console.WriteLine("Участник {0} сделал {1} попыток", personWithMaxNumberOfAttempts, maxNumberOfAttempts);
-
-
-			// Statistics on questions
-			var qStatistics = new Dictionary<string, QuestionStatistics>();
-
-			foreach (var testResult in data)
-			{
-				foreach (var answer in testResult.Answers)
-				{
-					if (!qStatistics.ContainsKey(answer.Question.QuestionText))
-						qStatistics.Add(answer.Question.QuestionText, new QuestionStatistics());
-
-					if (answer.AnswerIndex == answer.Question.CorrectAnswerIndex)
-						qStatistics[answer.Question.QuestionText].RightAnswersAmount++;
-					else
-						qStatistics[answer.Question.QuestionText].WrongAnswersAmount++;
-				}
-			}
-
-			foreach (var el in qStatistics)
+			Console.WriteLine("Статистика вопросов:");
+			foreach (var el in report.QuestionStatistics)
 			{
 				Console.WriteLine("Вопрос: {0}\nПравильных ответов: {1}, неправильных: {2}, всего: {3}",
 					el.Key, el.Value.RightAnswersAmount, el.Value.WrongAnswersAmount,
