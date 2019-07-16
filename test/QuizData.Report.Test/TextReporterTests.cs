3f54f568363e4b9a5276c891f8a5dd38ef49b575
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2019-07-16 17:34:21 +0300

    fixed formatting

--- test/QuizData.Report.Test/TextReporterTests.cs
+++ test/QuizData.Report.Test/TextReporterTests.cs
@@ -9,68 +9,68 @@ namespace QuizData.Report.Test
 {
     public class TextReporterTests
     {
-		private DataAnalyserReport _dataAnalyserReport { get; } = new DataAnalyserReport
-		{
-			TotalAmountOfTests = 1U,
-			AmountOfAttempts = new Dictionary<string, uint>()
-				{
-					{ "email@site.com", 1 }
-				},
-			ResultDistribution = new Dictionary<uint, uint>()
-				{
-					{ 60U, 1U }
-				},
-			QuestionStatistics = new Dictionary<string, QuestionStatistics>()
-				{
-					{ "Мейстеры Цитадели изобрели бургеры и спешат поделиться своим открытием со всеми. Но так ли хорош рецепт?",
-					new QuestionStatistics
-					{
-						AnswersDistribution = new uint[] {0, 0, 1, 0},
-						RightAnswerIndex = 2,
-						RightAnswersAmount = 1,
-						WrongAnswersAmount = 0
-					}
-					},
-					{ "Багет - это хлеб, а хлеб ? это багет?",
-					new QuestionStatistics
-					{
-						AnswersDistribution = new uint[] {0, 0, 0, 1},
-						RightAnswerIndex = 0,
-						RightAnswersAmount = 0,
-						WrongAnswersAmount = 1
-					}
-					},
-					{ "Путник знает, что и как заказать в таверне.",
-					new QuestionStatistics
-					{
-						AnswersDistribution = new uint[] {1, 0, 0, 0},
-						RightAnswerIndex = 0,
-						RightAnswersAmount = 1,
-						WrongAnswersAmount = 0
-					}
-					},
-					{ "Тормунд и Джон решили покушать бутербродов. Джон маленький и хочет скушать 2 бутерброда, а Тормунд большой и хочет 4. Но у Джона только 1 кусочек хлеба, а у Тормунда - только 3. Сколько кусочков хлеба им не хватает?",
-					new QuestionStatistics
-					{
-						AnswersDistribution = new uint[] {0, 0, 1, 0},
-						RightAnswerIndex = 2,
-						RightAnswersAmount = 1,
-						WrongAnswersAmount = 0
-					}
-					},
-					{ "Мелисандра хорошо управляется с магией. Получится ли у нас? Что будет выведено в консоль после расфокусировки input элемента?",
-					new QuestionStatistics
-					{
-						AnswersDistribution = new uint[] {1, 0, 0, 0},
-						RightAnswerIndex = 2,
-						RightAnswersAmount = 0,
-						WrongAnswersAmount = 1
-					}
-					}
-				}
-		};
+        private DataAnalyserReport _dataAnalyserReport { get; } = new DataAnalyserReport
+        {
+            TotalAmountOfTests = 1U,
+            AmountOfAttempts = new Dictionary<string, uint>()
+                {
+                    { "email@site.com", 1 }
+                },
+            ResultDistribution = new Dictionary<uint, uint>()
+                {
+                    { 60U, 1U }
+                },
+            QuestionStatistics = new Dictionary<string, QuestionStatistics>()
+                {
+                    { "Мейстеры Цитадели изобрели бургеры и спешат поделиться своим открытием со всеми. Но так ли хорош рецепт?",
+                    new QuestionStatistics
+                    {
+                        AnswersDistribution = new uint[] {0, 0, 1, 0},
+                        RightAnswerIndex = 2,
+                        RightAnswersAmount = 1,
+                        WrongAnswersAmount = 0
+                    }
+                    },
+                    { "Багет - это хлеб, а хлеб ? это багет?",
+                    new QuestionStatistics
+                    {
+                        AnswersDistribution = new uint[] {0, 0, 0, 1},
+                        RightAnswerIndex = 0,
+                        RightAnswersAmount = 0,
+                        WrongAnswersAmount = 1
+                    }
+                    },
+                    { "Путник знает, что и как заказать в таверне.",
+                    new QuestionStatistics
+                    {
+                        AnswersDistribution = new uint[] {1, 0, 0, 0},
+                        RightAnswerIndex = 0,
+                        RightAnswersAmount = 1,
+                        WrongAnswersAmount = 0
+                    }
+                    },
+                    { "Тормунд и Джон решили покушать бутербродов. Джон маленький и хочет скушать 2 бутерброда, а Тормунд большой и хочет 4. Но у Джона только 1 кусочек хлеба, а у Тормунда - только 3. Сколько кусочков хлеба им не хватает?",
+                    new QuestionStatistics
+                    {
+                        AnswersDistribution = new uint[] {0, 0, 1, 0},
+                        RightAnswerIndex = 2,
+                        RightAnswersAmount = 1,
+                        WrongAnswersAmount = 0
+                    }
+                    },
+                    { "Мелисандра хорошо управляется с магией. Получится ли у нас? Что будет выведено в консоль после расфокусировки input элемента?",
+                    new QuestionStatistics
+                    {
+                        AnswersDistribution = new uint[] {1, 0, 0, 0},
+                        RightAnswerIndex = 2,
+                        RightAnswersAmount = 0,
+                        WrongAnswersAmount = 1
+                    }
+                    }
+                }
+        };
 
-		private string _textReporterOutput =
+        private string _textReporterOutput =
 @"Всего тестов: 1
 Количество уникальных e-mail'ов: 1
 
@@ -103,7 +103,7 @@ namespace QuizData.Report.Test
 
 ";
 
-		[Fact]
+        [Fact]
         public void TextReporterTest()
         {
             using (var stream = new MemoryStream())
