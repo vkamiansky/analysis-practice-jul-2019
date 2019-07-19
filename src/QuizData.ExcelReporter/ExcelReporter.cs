53a262ffa27caf2d90fb249cebaf9b44b995e03a
Shurron <antr01@yandex.ru>
2019-07-19 17:02:28 +0300

    Refactoring

--- src/QuizData.ExcelReporter/ExcelReporter.cs
+++ src/QuizData.ExcelReporter/ExcelReporter.cs
@@ -10,134 +10,96 @@ namespace QuizData.ExcelReport
 	public class ExcelReporter
 	{
         private ExcelPackage _package;
-        private ExcelWorksheet _main;
-        private ExcelWorksheet _temp;
-        private ExcelWorksheet _questions;
-        private int _currentLine;
-        private int _tempCurrentLine;
-        private int _questionsCurrentLine;
+        private ExcelWorksheetWrapper _main;
+        private ExcelWorksheetWrapper _temp;
+        private ExcelWorksheetWrapper _questions;
 
         public ExcelReporter()
         {
             _package = new ExcelPackage();
-            _temp = _package.Workbook.Worksheets.Add("_Temp");
-            _main = _package.Workbook.Worksheets.Add("Главная");
-            _questions = _package.Workbook.Worksheets.Add("Вопросы");
-            _currentLine = 1;
-            _tempCurrentLine = 1;
-            _questionsCurrentLine = 1;
+            _temp = new ExcelWorksheetWrapper(_package.Workbook.Worksheets.Add("_Temp"));
+            _main = new ExcelWorksheetWrapper(_package.Workbook.Worksheets.Add("Главная"));
+            _questions = new ExcelWorksheetWrapper(_package.Workbook.Worksheets.Add("Вопросы"));
         }
 
         public void BuildAttemptDistributionChart(DataAnalyserReport report)
         {
-            var startLineTemp = _tempCurrentLine;
+            _temp.SetPos1();
 
             for (var i = 0; i < report.AttemptDistribution.Length; i++)
             {
                 if (report.AttemptDistribution[i] != 0)
                 {
-                    _temp.Cells[_tempCurrentLine, 1].Value = i + 1;
-                    _temp.Cells[_tempCurrentLine, 2].Value = report.AttemptDistribution[i];
-                    _tempCurrentLine++;
+                    _temp.Write(i + 1);
+                    _temp.WriteLine(report.AttemptDistribution[i]);
                 }
             }
 
-            var chart = _main.Drawings.AddChart("AttemptDistribution",
-                OfficeOpenXml.Drawing.Chart.eChartType.ColumnClustered);
-            chart.Title.Text = "Распределение попыток";
-            var address1 = string.Format("B{0}:B{1}", startLineTemp, _tempCurrentLine - 1);
-            var address2 = string.Format("A{0}:A{1}", startLineTemp, _tempCurrentLine - 1);
-            chart.Series.Add(ExcelRange.GetFullAddress("_Temp", address1),
-                ExcelRange.GetFullAddress("_Temp", address2));
-            chart.Legend.Remove();
-
-            chart.From.Row = _currentLine;
-            chart.To.Row = _currentLine + 10;
-            _currentLine += 11;
+            _temp.GoBack();
+            _temp.SetPos2();
+            _temp.WriteLine();
+            _temp.CreateChart("AttemptDistribution", "Распределение попыток", _main);
         }
 
         public void BuildResultDistributionChart(DataAnalyserReport report)
         {
-            var startLineTemp = _tempCurrentLine;
+            _temp.SetPos1();
 
             var list = report.ResultDistribution.ToList();
             list.Sort((pair1, pair2) => pair1.Key.CompareTo(pair2.Key));
 
             foreach (var el in list)
             {
-                _temp.Cells[_tempCurrentLine, 1].Value = el.Key;
-                _temp.Cells[_tempCurrentLine, 2].Value = el.Value;
-                _tempCurrentLine++;
+                _temp.Write(el.Key);
+                _temp.WriteLine(el.Value);
             }
 
-            var chart = _main.Drawings.AddChart("ResultDistribution",
-                OfficeOpenXml.Drawing.Chart.eChartType.ColumnClustered);
-            chart.Title.Text = "Распределение результатов";
-            var address1 = string.Format("B{0}:B{1}", startLineTemp, _tempCurrentLine - 1);
-            var address2 = string.Format("A{0}:A{1}", startLineTemp, _tempCurrentLine - 1);
-            chart.Series.Add(ExcelRange.GetFullAddress("_Temp", address1),
-                ExcelRange.GetFullAddress("_Temp", address2));
-            chart.Legend.Remove();
-
-            chart.From.Row = _currentLine;
-            chart.To.Row = _currentLine + 10;
-            _currentLine += 11;
+            _temp.GoBack();
+            _temp.SetPos2();
+            _temp.WriteLine();
+            _temp.CreateChart("ResultDistribution", "Распределение результатов", _main);
         }
 
         public void Question(KeyValuePair<string, QuestionStatistics> qStatistics)
         {
-            _questions.Cells[_questionsCurrentLine, 1].Value = qStatistics.Key;
-            _questionsCurrentLine++;
+            _questions.WriteLine(qStatistics.Key);
 
-            _questions.Cells[_questionsCurrentLine, 1].Value = "Правильных ответов:";
-            _questions.Cells[_questionsCurrentLine, 2].Value = qStatistics.Value.RightAnswersAmount;
-            _questionsCurrentLine++;
+            _questions.Write("Правильных ответов:");
+            _questions.WriteLine(qStatistics.Value.RightAnswersAmount);
 
-            _questions.Cells[_questionsCurrentLine, 1].Value = "Неправильных ответов:";
-            _questions.Cells[_questionsCurrentLine, 2].Value = qStatistics.Value.WrongAnswersAmount;
-            _questionsCurrentLine++;
+            _questions.Write("Неправильных ответов:");
+            _questions.WriteLine(qStatistics.Value.WrongAnswersAmount);
 
-            _questions.Cells[_questionsCurrentLine, 1].Value = "Всего ответов:";
-            _questions.Cells[_questionsCurrentLine, 2].Value = qStatistics.Value.RightAnswersAmount +
-                qStatistics.Value.WrongAnswersAmount;
-            _questionsCurrentLine++;
+            _questions.Write("Всего ответов:");
+            _questions.WriteLine(qStatistics.Value.RightAnswersAmount +
+                qStatistics.Value.WrongAnswersAmount);
 
-            _questions.Cells[_questionsCurrentLine, 1].Value = "Правильный ответ:";
-            _questions.Cells[_questionsCurrentLine, 2].Value = qStatistics.Value.RightAnswerIndex + 1;
-            _questionsCurrentLine++;
+            _questions.Write("Правильный ответ:");
+            _questions.WriteLine(qStatistics.Value.RightAnswerIndex + 1);
 
-            var startLineTemp = _tempCurrentLine;
+            _temp.SetPos1();
 
             for (var i = 0; i < qStatistics.Value.AnswersDistribution.Length; i++)
             {
-                _temp.Cells[_tempCurrentLine, 1].Value = i + 1;
-                _temp.Cells[_tempCurrentLine, 2].Value = qStatistics.Value.AnswersDistribution[i];
-                _tempCurrentLine++;
+                _temp.Write(i + 1);
+                _temp.WriteLine(qStatistics.Value.AnswersDistribution[i]);
             }
 
-            var chart = _questions.Drawings.AddChart("QuestionStatistics" + qStatistics.Key.GetHashCode(),
-                OfficeOpenXml.Drawing.Chart.eChartType.ColumnClustered);
-            chart.Title.Text = "Ответы пользователей";
-            var address1 = string.Format("B{0}:B{1}", startLineTemp, _tempCurrentLine - 1);
-            var address2 = string.Format("A{0}:A{1}", startLineTemp, _tempCurrentLine - 1);
-            chart.Series.Add(ExcelRange.GetFullAddress("_Temp", address1),
-                ExcelRange.GetFullAddress("_Temp", address2));
-            chart.Legend.Remove();
-
-            chart.From.Row = _questionsCurrentLine;
-            chart.To.Row = _questionsCurrentLine + 10;
-            _questionsCurrentLine += 12;
+            _temp.GoBack();
+            _temp.SetPos2();
+            _temp.WriteLine();
+            _temp.CreateChart("QuestionStatistics" + qStatistics.Key.GetHashCode(),
+                "Ответы пользователей", _questions);
+            _questions.WriteLine();
         }
 
         public void ToFile(string path, DataAnalyserReport report)
 		{
-            _main.Cells[_currentLine, 1].Value = "Всего тестов: ";
-			_main.Cells[_currentLine, 2].Value = report.TotalAmountOfTests;
-            _currentLine++;
+            _main.Write("Всего тестов:");
+			_main.WriteLine(report.TotalAmountOfTests);
 
-            _main.Cells[_currentLine, 1].Value = "Количество уникальных e-mail'ов: ";
-            _main.Cells[_currentLine, 2].Value = report.AmountOfUniqueEmails;
-            _currentLine++;
+            _main.Write("Количество уникальных e-mail'ов:");
+            _main.WriteLine(report.AmountOfUniqueEmails);
 
             BuildAttemptDistributionChart(report);
             BuildResultDistributionChart(report);
@@ -145,36 +107,36 @@ namespace QuizData.ExcelReport
             foreach (var el in report.QuestionStatistics)
                 Question(el);
 
-			//var max = 0U;
-			//var userWithMax = "";
-			//foreach (var pStatistics in report.PersonStatistics)
-			//{
-			//	if (pStatistics.Value.AmountOfAttempts > max)
-			//	{
-			//		max = pStatistics.Value.AmountOfAttempts;
-			//		userWithMax = pStatistics.Key;
-			//	}
-			//}
+            //var max = 0U;
+            //var userWithMax = "";
+            //foreach (var pStatistics in report.PersonStatistics)
+            //{
+            //	if (pStatistics.Value.AmountOfAttempts > max)
+            //	{
+            //		max = pStatistics.Value.AmountOfAttempts;
+            //		userWithMax = pStatistics.Key;
+            //	}
+            //}
 
-			
 
-            
 
-   //         _main.Cells[20, 1].Value = string.Format("Результаты пользователя {0}", userWithMax);
-			//currentLineNumber = 21;
-			//for (var i = 0; i < report.PersonStatistics[userWithMax].Results.Count; i++)
-			//{
-			//	_main.Cells[currentLineNumber, 1].Value = i + 1;
-			//	_main.Cells[currentLineNumber, 2].Value = report.PersonStatistics[userWithMax].Results[i];
-			//	currentLineNumber++;
-			//}
 
-			//var chart2 = _main.Drawings.AddChart("chart2", OfficeOpenXml.Drawing.Chart.eChartType.Line);
-			//var chartSerie = chart2.Series.Add(ExcelRange.GetAddress(21, 2, 62, 2), ExcelRange.GetAddress(21, 1, 62, 1));
-			//var trend = chartSerie.TrendLines.Add(OfficeOpenXml.Drawing.Chart.eTrendLine.Linear);
-			//trend.DisplayRSquaredValue = false;
 
-			_package.SaveAs(new FileInfo(path));
+            //         _main.Cells[20, 1].Value = string.Format("Результаты пользователя {0}", userWithMax);
+            //currentLineNumber = 21;
+            //for (var i = 0; i < report.PersonStatistics[userWithMax].Results.Count; i++)
+            //{
+            //	_main.Cells[currentLineNumber, 1].Value = i + 1;
+            //	_main.Cells[currentLineNumber, 2].Value = report.PersonStatistics[userWithMax].Results[i];
+            //	currentLineNumber++;
+            //}
+
+            //var chart2 = _main.Drawings.AddChart("chart2", OfficeOpenXml.Drawing.Chart.eChartType.Line);
+            //var chartSerie = chart2.Series.Add(ExcelRange.GetAddress(21, 2, 62, 2), ExcelRange.GetAddress(21, 1, 62, 1));
+            //var trend = chartSerie.TrendLines.Add(OfficeOpenXml.Drawing.Chart.eTrendLine.Linear);
+            //trend.DisplayRSquaredValue = false;
+
+            _package.SaveAs(new FileInfo(path));
 		}
 	}
 }
---
+++ src/QuizData.ExcelReporter/ExcelWorksheetWrapper.cs
@@ -0,0 +1,104 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Drawing.Chart;
+
+namespace QuizData.ExcelReport
+{
+    public class ExcelWorksheetWrapper
+    {
+        public ExcelWorksheet Worksheet { get; }
+
+        private int _currentLine;
+        private int _currentColumn;
+
+        private int _pos1Line;
+        private int _pos1Column;
+
+        private int _pos2Line;
+        private int _pos2Column;
+
+        private int _previousStepLine;
+        private int _previousStepColumn;
+        private bool _canGoBack;
+
+        public ExcelWorksheetWrapper(ExcelWorksheet worksheet)
+        {
+            Worksheet = worksheet;
+            _currentLine = 1;
+            _currentColumn = 1;
+        }
+
+        private void CopyToPrevious()
+        {
+            _previousStepLine = _currentLine;
+            _previousStepColumn = _currentColumn;
+            _canGoBack = true;
+        }
+
+        public void Write(object value)
+        {
+            Worksheet.Cells[_currentLine, _currentColumn].Value = value;
+            CopyToPrevious();
+            _currentColumn++;
+        }
+
+        public void WriteLine()
+        {
+            CopyToPrevious();
+            _currentLine++;
+            _currentColumn = 1;
+        }
+
+        public void WriteLine(object value)
+        {
+            Worksheet.Cells[_currentLine, _currentColumn].Value = value;
+            CopyToPrevious();
+            _currentLine++;
+            _currentColumn = 1;
+        }
+
+        public void GoBack()
+        {
+            if (!_canGoBack)
+                throw new System.Exception("Something went wrong...");
+
+            _currentColumn = _previousStepColumn;
+            _currentLine = _previousStepLine;
+
+            _canGoBack = false;
+        }
+
+        public void SetPos1()
+        {
+            _pos1Line = _currentLine;
+            _pos1Column = _currentColumn;
+        }
+
+        public void SetPos2()
+        {
+            _pos2Line = _currentLine;
+            _pos2Column = _currentColumn;
+        }
+
+        public void CreateChart(string chartName, string chartTitle, ExcelWorksheetWrapper to)
+        {
+            var chart = to.Worksheet.Drawings.AddChart(chartName, eChartType.ColumnClustered);
+            chart.Title.Text = chartTitle;
+            var address1 = string.Format("{0}{1}:{2}{3}",
+                (char)(_pos2Column + 64), _pos1Line, (char)(_pos2Column + 64), _pos2Line);
+            var address2 = string.Format("{0}{1}:{2}{3}",
+                (char)(_pos1Column + 64), _pos1Line, (char)(_pos1Column + 64), _pos2Line);
+            chart.Series.Add(ExcelRange.GetFullAddress(Worksheet.Name, address1),
+                ExcelRange.GetFullAddress(Worksheet.Name, address2));
+            chart.Legend.Remove();
+
+            to.PlaceChart(chart);
+        }
+
+        public void PlaceChart(ExcelChart chart)
+        {
+            chart.From.Row = _currentLine;
+            chart.To.Row = _currentLine + 10;
+            _currentLine += 11;
+        }
+    }
+}
