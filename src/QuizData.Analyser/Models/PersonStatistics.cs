fcbed7a6a5e4a8772e04557a28d987766cc5816c
Shurron <antr01@yandex.ru>
2019-07-22 13:10:13 +0300

    Added RMS error

--- src/QuizData.Analyser/Models/PersonStatistics.cs
+++ src/QuizData.Analyser/Models/PersonStatistics.cs
@@ -5,7 +5,7 @@ namespace QuizData.Analyser.Models
     public class PersonStatistics
     {
         public List<uint> Results { get; set; } = new List<uint>();
-        public (double K, double B)? AdditionalInfo { get; set; }
+        public (double K, double B, double R)? AdditionalInfo { get; set; }
 
         public uint AmountOfAttempts
         {
