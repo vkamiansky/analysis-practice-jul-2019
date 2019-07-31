de4b6093af16b4bfcfae9b4e151c4e8ac1997005
Shurron <antr01@yandex.ru>
2019-07-31 17:20:04 +0300

    One unit test for DataTransformer

--- test/QuizData.Report.Test/TextReporterTests.cs
+++ test/QuizData.Report.Test/TextReporterTests.cs
@@ -1,4 +1,3 @@
-using QuizData.Analyser.Models;
 using QuizData.Analyser.Models.DataBlocks;
 using QuizData.TextReport;
 using System;
@@ -10,74 +9,6 @@ namespace QuizData.Report.Test
 {
     public class TextReporterTests
     {
-        private DataAnalyserReport _dataAnalyserReport { get; } = new DataAnalyserReport
-        {
-            TotalAmountOfTests = 1U,
-            PersonStatistics = new Dictionary<string, PersonStatistics>()
-            {
-                { "email@site.com", new PersonStatistics()
-                    {
-                        Results = new List<uint>()
-                        {
-                            60U
-                        }
-                    }
-                }
-            },
-            ResultDistribution = new Dictionary<uint, uint>()
-            {
-                { 60U, 1U }
-            },
-            QuestionStatistics = new Dictionary<string, QuestionStatistics>()
-            {
-                { "Мейстеры Цитадели изобрели бургеры и спешат поделиться своим открытием со всеми. Но так ли хорош рецепт?",
-                new QuestionStatistics
-                {
-                    AnswersDistribution = new uint[] {0, 0, 1, 0},
-                    RightAnswerIndex = 2,
-                    RightAnswersAmount = 1,
-                    WrongAnswersAmount = 0
-                }
-                },
-                { "Багет - это хлеб, а хлеб ? это багет?",
-                new QuestionStatistics
-                {
-                    AnswersDistribution = new uint[] {0, 0, 0, 1},
-                    RightAnswerIndex = 0,
-                    RightAnswersAmount = 0,
-                    WrongAnswersAmount = 1
-                }
-                },
-                { "Путник знает, что и как заказать в таверне.",
-                new QuestionStatistics
-                {
-                    AnswersDistribution = new uint[] {1, 0, 0, 0},
-                    RightAnswerIndex = 0,
-                    RightAnswersAmount = 1,
-                    WrongAnswersAmount = 0
-                }
-                },
-                { "Тормунд и Джон решили покушать бутербродов. Джон маленький и хочет скушать 2 бутерброда, а Тормунд большой и хочет 4. Но у Джона только 1 кусочек хлеба, а у Тормунда - только 3. Сколько кусочков хлеба им не хватает?",
-                new QuestionStatistics
-                {
-                    AnswersDistribution = new uint[] {0, 0, 1, 0},
-                    RightAnswerIndex = 2,
-                    RightAnswersAmount = 1,
-                    WrongAnswersAmount = 0
-                }
-                },
-                { "Мелисандра хорошо управляется с магией. Получится ли у нас? Что будет выведено в консоль после расфокусировки input элемента?",
-                new QuestionStatistics
-                {
-                    AnswersDistribution = new uint[] {1, 0, 0, 0},
-                    RightAnswerIndex = 2,
-                    RightAnswersAmount = 0,
-                    WrongAnswersAmount = 1
-                }
-                }
-            }
-        };
-
         private List<IDataBlock> _mainData = new List<IDataBlock>()
         {
             new ScalarDataBlock(1, "Всего тестов:"),
